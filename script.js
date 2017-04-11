da57b2e1ca89275b66fe8f3f003e89a1d16c0c6c
Breana Wallace <breanawallace131@gmail.com>
2017-04-10 20:43:34 -0400

    first upload, centered number buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$(function() {
+	    $('.center').css({
+	        'position' : 'absolute',
+	        'left' : '50%',
+	        'top' : '50%',
+	        'margin-left' : -$('.center').outerWidth()/2,
+	        'margin-top' : -$('.center').outerHeight()/2
+	    });
+	});
+});
