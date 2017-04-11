6772ab57cd1683733101f77ea47e7f009c281905
Breana Wallace <breanawallace131@gmail.com>
2017-04-11 06:28:43 -0400

    buttons all the samze size, numbers assigned to a value onclickand they push to an integer array which saves the button push. Next I will work on creating functions for the operator buttons to do things in that array and update the answer variable as I go

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+	//center the calculator
 	$(function() {
 	    $('.center').css({
 	        'position' : 'absolute',
@@ -9,4 +10,62 @@ $(document).ready(function(){
 	        'margin-top' : -$('.center').outerHeight()/2
 	    });
 	});
+
+	//create the array that will be used in functions
+	var integerArray = [];
+	
+	//changing variables as the code runs
+	var integer = 0;
+
+	var answer = 0;
+
+	//have event listeners to store data in the arrays above
+	
+	//Numbers
+	$('#one').click(function(){
+		var integer = 1;
+		integerArray.push(integer);
+	})
+	$('#two').click(function(){
+		var integer = 2;
+		integerArray.push(integer);
+	})
+	$('#three').click(function(){
+		var integer = 3;
+		integerArray.push(integer);
+	})
+	$('#four').click(function(){
+		var integer = 4;
+		integerArray.push(integer);
+	})
+	$('#five').click(function(){
+		var integer = 5;
+		integerArray.push(integer);
+	})
+	$('#six').click(function(){
+		var integer = 6;
+		integerArray.push(integer);
+	})
+	$('#seven').click(function(){
+		var integer = 7;
+		integerArray.push(integer);
+	})
+	$('#eight').click(function(){
+		var integer = 8;
+		integerArray.push(integer);
+	})
+	$('#nine').click(function(){
+		var integer = 9;
+		integerArray.push(integer);
+	})
+
+	console.log(integerArray);
+
+
+	//functions for + - * / using integerArray
+	
+	//add them
+	
+	
+
 });
