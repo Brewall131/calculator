8e6b1f22e3d0ac27f7a26bbfab2ac77ab5d72dc1
Breana Wallace <breanawallace131@gmail.com>
2017-04-12 22:14:54 -0400

    the calculator will spit back the answer once the equal sign is pushed. Currntly this shows up in the console, next I would like to add a clear button and a screen for the answer to show up on

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+var a = 2;
+
+var operand1 = "";
+
+var operator = "";
+
+var operand2 = "";
+	
+var answer = "";
+
 $(document).ready(function(){
 
 	//center the calculator
@@ -11,71 +21,147 @@ $(document).ready(function(){
 	    });
 	});
 
-	//create the array that will be used in functions
-	var integerArray = [];
-	
+	//variables to plaace the integers and operators into 
 	//changing variables as the code runs
-	var integer = 0;
-
-	var answer = 0;
-
-	//have event listeners to store data in the arrays above
 	
+	
+	//CURRENT PROBLEM: numbers are not seperating out when an operator is entered.
+
+	//will need to destringify next.
+
+	//event listeners to store data in the variables above
 	//Numbers
+	$('#zero').click(function(){
+		var integer = "0";
+
+		if (operator == "") {
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
+	})
 	$('#one').click(function(){
-		var integer = 1;
-		integerArray.push(integer);
+		var integer = "1";
+
+		if (operator == "") {
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#two').click(function(){
-		var integer = 2;
-		integerArray.push(integer);
+		var integer = "2";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#three').click(function(){
-		var integer = 3;
-		integerArray.push(integer);
+		var integer = "3";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#four').click(function(){
-		var integer = 4;
-		integerArray.push(integer);
+		var integer = "4";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#five').click(function(){
-		var integer = 5;
-		integerArray.push(integer);
+		var integer = "5";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#six').click(function(){
-		var integer = 6;
-		integerArray.push(integer);
+		var integer = "6";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#seven').click(function(){
-		var integer = 7;
-		integerArray.push(integer);
+		var integer = "7";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#eight').click(function(){
-		var integer = 8;
-		integerArray.push(integer);
+		var integer = "8";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 	$('#nine').click(function(){
-		var integer = 9;
-		integerArray.push(integer);
+		var integer = "9";
+
+		if (operator == "" ){
+			operand1 += integer;
+		}
+		else {
+			operand2 += integer;
+		}
+		
 	})
 
-	console.log(integerArray);
 
-
-	//functions for + - * / using integerArray
-	
-	//add them
+	//add operators to the operator variable string
 	$('#plus').click(function(){
-		var total = integerArray.reduce(add, 0)
-			function add(a, b) {
-				return a + b
-			}
-		answer = total;
+		 operator = "+";
+	})
+	$('#minus').click(function(){
+		 operator = "-";
+	})
+	$('#multiply').click(function(){
+		 operator = "*";
+	})
+	$('#divide').click(function(){
+		 operator = "/";
 	})
 
-	//show current answer
+	//show current answer when = is hit
 	$('#equals').click(function(){
-		console.log(answer);
+		 answer = operand1.concat(operator, operand2);
+		 answer = eval(answer);
+		 console.log(answer);
 	})
 	
 
